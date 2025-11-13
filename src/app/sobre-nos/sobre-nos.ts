017e8ed02563cefb86276626cb249da31ba748f1
yukari4u <mkato4@gmail.com>
2025-11-13 01:05:03 -0300

    Sobre nos

--- src/app/sobre-nos/sobre-nos.ts
+++ src/app/sobre-nos/sobre-nos.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 
+interface TeamMember {
+  name: string;
+  role: string[];
+  imageUrl: string;
+  linkedinUrl: string;
+  githubUrl: string;
+}
+
 @Component({
   selector: 'app-sobre-nos',
   standalone: false,
@@ -7,5 +15,36 @@ import { Component } from '@angular/core';
   styleUrl: './sobre-nos.css'
 })
 export class SobreNos {
+
+    teamMembers: TeamMember[] = [
+    {
+      name: 'Danielle Namie Yoshimura',
+      role: ['Gestor do Projeto', 'Scrum Master', 'Analista de BI'],
+      imageUrl: 'assets/img/team/membro1.png',
+      linkedinUrl: 'https://linkedin.com/in/...',
+      githubUrl: 'https://github.com/...'
+    },
+    {
+      name: 'Gabriel Capelini de Oliveira',
+      role: ['Dev Back-End', 'Analista de Viabilidade', 'Especialista em Deploy'],
+      imageUrl: 'assets/img/team/membro2.png',
+      linkedinUrl: 'https://linkedin.com/in/...',
+      githubUrl: 'https://github.com/...'
+    },
+    {
+      name: 'Júlia Moraes Martins',
+      role: ['Designer', 'Analista de Requisitos', 'Product Owner', 'Consultora de Usabilidade'],
+      imageUrl: 'assets/img/team/membro3.png',
+      linkedinUrl: 'https://linkedin.com/in/...',
+      githubUrl: 'https://github.com/...'
+    },
+    {
+      name: 'Melissa Yukari Kato',
+      role: ['Dev Front-End', 'Engenheira de Testes', 'Analista de Suporte'],
+      imageUrl: 'assets/img/team/membro4.png',
+      linkedinUrl: 'https://linkedin.com/in/...',
+      githubUrl: 'https://github.com/...'
+    }
+  ];
 
 }
