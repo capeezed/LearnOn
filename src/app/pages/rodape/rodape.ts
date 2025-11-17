68354cb2fa36212ec68eeb1c378da4f84b7b1dce
Melissa Yukari Kato <mkato4@gmail.com>
2025-11-17 14:33:09 -0300

    Organização

--- src/app/pages/rodape/rodape.ts
+++ src/app/pages/rodape/rodape.ts
@@ -9,12 +9,8 @@ import { Component } from '@angular/core';
 
 export class Rodape {
   onSubmit() {
-    // Implemente aqui a lógica para enviar o e-mail
-    // para um serviço ou API.
     console.log('E-mail enviado para inscrição na newsletter!');
     alert('Obrigado por se inscrever! Fique ligado nas nossas ofertas.');
 
-    // Idealmente, você chamaria um Service aqui:
-    // this.newsletterService.subscribe(emailData).subscribe(...);
   }
 }
