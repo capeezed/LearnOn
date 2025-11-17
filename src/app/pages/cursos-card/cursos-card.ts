4c87c74d0a61518e45ba5f4bf626bf3bfbf1d2c9
Melissa Yukari Kato <mkato4@gmail.com>
2025-11-17 12:15:03 -0300

    Organização

--- src/app/pages/cursos-card/cursos-card.ts
+++ src/app/pages/cursos-card/cursos-card.ts
@@ -21,34 +21,27 @@ interface Curso{
 export class CursosCard {
   @Input() curso!: Curso;
 
-  // 2. Injete o CartService no construtor
   constructor(private gerenciamentoCarrinho: GerenciamentoCarrinho) { }
 
-  // Função para criar um array de estrelas para o *ngFor
   getNota(): number[] {
   return Array(this.curso.nota).fill(0);
   }
 
-  //função para formatar o preço em real
   formatarPreco(preco: number): string {
   return preco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
   }
 
-  //ação de click
   detalhesCurso(){
-  //implementar a navegação para a página de detalhes do curso
   console.log(`Ver detalhes do curso: ${this.curso.nome}`);
   }
 
   addCarrinho(): void {
     if (this.curso) {
-      // 3. Chama o método do serviço
       this.gerenciamentoCarrinho.addCarrinho(this.curso);
     }
   }
 
   verDetalhesCursos() {
-    // Implementar a navegação para a página de detalhes do curso
     console.log(`Ver detalhes do curso: ${this.curso.nome}`);
   }
 
