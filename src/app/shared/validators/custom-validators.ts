ba1b56ea7028f5c6e97e48d812503c36380c521c
Gabriel Capelini de Oliveira <158757773+capeezed@users.noreply.github.com>
2025-11-17 15:18:25 -0300

    Refactor passwordMatchValidator for clarity

--- src/app/shared/validators/custom-validators.ts
+++ src/app/shared/validators/custom-validators.ts
@@ -2,7 +2,7 @@ import { AbstractControl, ValidatorFn } from '@angular/forms';
 
 export const passwordMatchValidator = (senhaName: string, confirmName: string): ValidatorFn => {
   return (control: AbstractControl): { [key: string]: boolean } | null => {
-    
+
     const senha = control.get(senhaName);
     const confirmacaoSenha = control.get(confirmName);
 
@@ -13,18 +13,18 @@ export const passwordMatchValidator = (senhaName: string, confirmName: string): 
     if (!confirmacaoSenha.value || confirmacaoSenha.pristine) {
       return null;
     }
-    
+
     if (senha.value !== confirmacaoSenha.value) {
       confirmacaoSenha.setErrors({ ...confirmacaoSenha.errors, senhasDiferentes: true });
-      
-      return { 'senhasDiferentes': true };
-    } 
-    
+      return { senhasDiferentes: true };
+    }
+
     if (confirmacaoSenha.hasError('senhasDiferentes')) {
       const errors = { ...confirmacaoSenha.errors };
       delete errors['senhasDiferentes'];
       confirmacaoSenha.setErrors(Object.keys(errors).length > 0 ? errors : null);
     }
-    
+
+    return null;
   };
 };
