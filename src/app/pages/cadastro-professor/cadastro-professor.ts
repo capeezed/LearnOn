29082d75a9e2d30f06154d64e9cf1c32230e5595
Melissa Yukari Kato <mkato4@gmail.com>
2025-11-17 11:58:14 -0300

    Organização

--- src/app/pages/cadastro-professor/cadastro-professor.ts
+++ src/app/pages/cadastro-professor/cadastro-professor.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
-import { passwordMatchValidator } from '../../shared/validators/custom-validators'; // Importa o validador reutilizável
+import { passwordMatchValidator } from '../../shared/validators/custom-validators'; 
 
 @Component({
   selector: 'app-cadastro-professor',
@@ -16,7 +16,6 @@ export class CadastroProfessor implements OnInit {
   registerError: string = '';
   isLoading: boolean = false; 
 
-  // Listas estáticas para os campos de seleção
   areas = ['Desenvolvimento Web', 'Cloud Computing', 'Data Science', 'Design UX/UI', 'DevOps/Infra', 'Mobile', 'Outra'];
   experienciaOptions = ['1-3 anos', '3-5 anos', '5-10 anos', '10+ anos'];
 
@@ -27,22 +26,19 @@ export class CadastroProfessor implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    // Inicializa o formulário reativo com todas as validações
     this.registerForm = this.fb.group({
       nome: ['', [Validators.required, Validators.minLength(3)]],
       email: ['', [Validators.required, Validators.email]],
 
-      // CAMPOS DE QUALIFICAÇÃO
       areaPrincipal: ['', Validators.required],
       tempoExperiencia: ['', Validators.required],
       comprovacaoDidatica: ['', [Validators.required, Validators.minLength(100)]],
 
       senha: ['', [Validators.required, Validators.minLength(6)]],
       confirmacaoSenha: ['', [Validators.required]]
-    }, { validators: passwordMatchValidator }); // Aplica o validador no FormGroup
+    }, { validators: passwordMatchValidator }); 
   }
 
-  // Getters para facilitar o acesso aos controles no HTML
   get nome() { return this.registerForm.get('nome'); }
   get email() { return this.registerForm.get('email'); }
   get senha() { return this.registerForm.get('senha'); }
@@ -60,7 +56,6 @@ export class CadastroProfessor implements OnInit {
 
       const professorData = this.registerForm.value;
       
-      // Chamada para a rota de cadastro de professor via AuthService
       this.authService.registerProfessor(professorData).subscribe({
         next: (response) => {
           alert('Pré-cadastro de professor efetuado! Aguarde a aprovação da nossa equipe.');
