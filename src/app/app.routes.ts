78be4fc2f73ef4de1e2b787978aef66ac55a9706
Suporte TI <vitor.delfino@urbes.com.br>
2026-03-10 12:08:26 -0300

    Home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,14 +6,16 @@ import { PedirCurso } from './pages/pedir-curso/pedir-curso';
 import { MeusCursos } from './pages/meus-cursos/meus-cursos';
 import { Curso } from './pages/curso/curso';
 import { Agenda } from './pages/agenda/agenda';
+import { Home } from './pages/home/home';
 
 export const routes: Routes = [
-  { path: '',          redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login',     component: Login },
-  { path: 'cadastro',  component: Cadastro },
-  { path: 'dashboard', component: Dashboard },
+  { path: '',            redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home',         component: Home },
+  { path: 'login',       component: Login },
+  { path: 'cadastro',    component: Cadastro },
+  { path: 'dashboard',   component: Dashboard },
   { path: 'pedir-curso', component: PedirCurso },
   { path: 'meus-cursos', component: MeusCursos },
-  { path: 'curso/:id', component: Curso },
-  { path: 'agenda',    component: Agenda },
-]
+  { path: 'curso/:id',   component: Curso },
+  { path: 'agenda',      component: Agenda },
+];
